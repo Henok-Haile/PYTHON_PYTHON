a44b8cc887ec8b240eb483c3acb1467ab0243a31
Henok-Haile <henokhaile53@gmail.com>
2023-07-19 07:50:58 +0300

    modified

--- ALX/Regular-Expresion/0-safe_print_list.py
+++ ALX/Regular-Expresion/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
     
     Args:
         my_list (list): The list from which the elements to be printed.
-        x (int): The number of elements to be printed.
+        x (int): The number of elements to be printed.code 
     """
     ret = 0
     for i in range(x):
