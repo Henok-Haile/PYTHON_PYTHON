9ca112fad8b9c868bf952f6e2c5f793b4dd1d9d1
Henok-Haile <henokhaile53@gmail.com>
2023-07-21 23:58:53 +0300

    oop

---
+++ ALX/python_classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py
+# Henok H Ghebreweldi <henokhaile53@gmail.com>
+"""Defines a class Square."""
+    
+
+class Square:
+    """Represents a Square class."""
+    pass
