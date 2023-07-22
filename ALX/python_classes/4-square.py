06ecb4b38e3a978ec7a793e119d94708bff2ac7e
Henok-Haile <henokhaile53@gmail.com>
2023-07-22 22:40:39 +0300

    area method and property decorator added

---
+++ ALX/python_classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# 4-square.py
+# Henok H Ghebreweldi <henokhaile53@gmail.com>
+"""Defines a Square Class."""
+
+
+class Square:
+    """Represents a Square class."""
+
+    def __init__(self, size=0):
+        """Initializes a new square.
+        
+        Args:
+            size (int): Size of the new square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the current size of the square."""
+        return (self.__size)
+    
+    @size.setter
+    def size(self, value):
+        """Sets the current size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current area of the square."""
+        return (self.__size * self.__size)
