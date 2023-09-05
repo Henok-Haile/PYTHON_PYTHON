7469a03fa1e52ee02ed7a22bb27602ddce31fb4d
Henok-Haile <henokhaile53@gmail.com>
2023-09-05 07:35:05 +0300

    techTFQ tutorial

---
+++ Neso Academy/exception1.py
@@ -0,0 +1,17 @@
+
+try:
+	# block of Code
+	pass 
+except:
+	# execute when error happens
+	pass
+else:
+	# excecute when there is no error
+	pass
+finally:
+	# always execute
+	pass
+
+
+
+raise exceptions: #user defined exceptions
