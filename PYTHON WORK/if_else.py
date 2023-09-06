64ec4c4ed189348ed8db10b8a08792bf0e637458
Henok-Haile <henokhaile53@gmail.com>
2023-09-06 17:10:40 +0300

    if else statement

---
+++ PYTHON WORK/if_else.py
@@ -0,0 +1,7 @@
+age = 16
+
+if age < 18:
+	print("You can't vote!")
+else:
+	print("You can vote!")
+print("Done!")
---
+++ PYTHON WORK/shorthand_if_else.py
@@ -0,0 +1,4 @@
+age = 16
+
+print("You can't vote!") if age < 18 else print("You can vote!")
+print("Done!")
