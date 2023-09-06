cf27113f9beac5e2a3840d5581e8eb557330e990
Henok-Haile <henokhaile53@gmail.com>
2023-09-06 16:56:45 +0300

    if statement

---
+++ PYTHON WORK/less_than_50.py
@@ -0,0 +1,4 @@
+x = 10
+if x < 50:
+	print("x is less than 50")
+print("End of the program")
---
+++ PYTHON WORK/nested_if.py
@@ -0,0 +1,5 @@
+x = 10
+if x < 50:
+	if x == 10:
+		print("x is equal to 10")
+print("End of the program")
