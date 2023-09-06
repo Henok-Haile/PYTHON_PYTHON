e9900ba494510579e0413207ad606e8443b844ef
Henok-Haile <henokhaile53@gmail.com>
2023-09-06 17:23:55 +0300

    if elif else statement

---
+++ PYTHON WORK/check_divisibility.py
@@ -0,0 +1,11 @@
+# WAP to test whether a number is divisible by 2 or 3
+
+num = 17 # test 10, 15, 17
+
+if num % 2 == 0:
+	print("Number is divisible by 2")
+elif num % 3 == 0:
+	print("Number is divisible by 3")
+else:
+	print("Number is neither divisible by 2 or 3")
+print("Done!")
---
+++ PYTHON WORK/if_elif_else.py
@@ -0,0 +1,9 @@
+x = 6
+
+if x == 5:
+	print("x is 5")
+elif x < 5:
+	print("x is less than 5")
+else:
+	print("x is greater than 5")
+print("Done!")
